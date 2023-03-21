7df75a0cec52135d31b50c0ab40846233ebe949e
PoorCodeWriter <102811406+PoorCodeWriter@users.noreply.github.com>
2023-03-21 12:52:31 -0400

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "API-KEY"
 while True:
     x = input("Ask me anything: ")
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": x}])
